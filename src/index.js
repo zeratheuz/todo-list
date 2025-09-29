67e0c7507f0b439d3ecded4ad64e123a4275e5b9
zeratheuz <zeratheuz@gmail.com>
2025-09-29 17:46:05 -0300

    Update CSS and JS

--- src/index.js
+++ src/index.js
@@ -24,14 +24,14 @@ function displayTasks() {
   tasks.forEach(task => {
     const liTask = document.createElement("li")
     ulTask.appendChild(liTask)
-    const divTask = document.createElement("div")
-    divTask.classList.add("task-details")
-    liTask.appendChild(divTask)
     const taskCheck = document.createElement("input")
     taskCheck.type = "checkbox"
     taskCheck.id = task.id
     taskCheck.name = task.title
-    divTask.appendChild(taskCheck)
+    liTask.appendChild(taskCheck)
+    const divTask = document.createElement("div")
+    divTask.classList.add("task-details")
+    liTask.appendChild(divTask)
     const taskTitle = document.createElement("h3")
     taskTitle.textContent = task.title
     divTask.appendChild(taskTitle)
@@ -46,9 +46,9 @@ function displayTasks() {
   })
 }
 
-tasks.push(new Task("Run", "just do it", "09/29/25", "2"))
+// tasks.push(new Task("Run", "just do it", "09/29/25", "2"))
 tasks.push(new Task("Train", "Sharpen your skills", "09/30/25", "3"))
-// tasks.push(new Task("Patrol", "Gotham needs you", "10/01/25", "2"))
+tasks.push(new Task("Patrol", "Gotham needs you", "10/01/25", "2"))
 // tasks.push(new Task("Investigate", "Track Joker's whereabouts", "10/02/25", "3"))
 // tasks.push(new Task("Rest", "Even Batman needs sleep", "10/03/25", "1"))
 // tasks.push(new Task("Upgrade Batmobile", "Install new AI system", "10/04/25", "2"))

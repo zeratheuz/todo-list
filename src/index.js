5d00c0ae686ce4485cc4d3717eb1d6e74d6d338e
zeratheuz <zeratheuz@gmail.com>
2025-10-02 16:45:35 -0300

    Update HTML and JS

--- src/index.js
+++ src/index.js
@@ -1,67 +1,72 @@
 import './style.css'
 import { createButtonAddTask } from './createButtonAddTask.js'
 
-const divMenu = document.querySelector("#menu")
-
 const divContent = document.querySelector("#content")
 
-export function displayTasks(divContent) {
+const btnHigh = document.querySelector("#high")
+btnHigh.addEventListener("click", () => {
+  displayTasks(divContent, 3)
+})
+
+export function displayTasks(divContent, filter = "allTasks") {
   const ulTask = document.querySelector("#ulTask")
   ulTask.innerHTML = ""
   Object.values(localStorage).forEach(value => {
     const task = JSON.parse(value)
-    const liTask = document.createElement("li")
-    switch (task.priority) {
-      case 1:
-        liTask.classList.add("green")
-        console.log(task.priority)
-        break
-      case 2:
-        liTask.classList.add("orange")
-        console.log(task.priority)
-        break
-      case 3:
-        liTask.classList.add("red")
-        console.log(task.priority)
-        break
-      default:
-        liTask.classList.add("blue")
-        console.log(task.priority)
-    }
-    ulTask.appendChild(liTask)
-    const taskCheck = document.createElement("input")
-    taskCheck.type = "checkbox"
-    taskCheck.id = task.id
-    taskCheck.name = task.title
-    console.log(task.priority)
-    taskCheck.addEventListener("mouseenter", () => {
-      taskCheck.checked = true
-    })
-    taskCheck.addEventListener("mouseleave", () => {
-      taskCheck.checked = false
-    })
-    taskCheck.addEventListener("click", () => {
-      localStorage.removeItem(task.id)
-      displayTasks(divContent)
-      const buttonAddTask = document.querySelector(".add-task")
-      divContent.removeChild(buttonAddTask)
-      createButtonAddTask(divContent)
-    })
-    liTask.appendChild(taskCheck)
-    const divTask = document.createElement("div")
-    divTask.classList.add("task-details")
-    liTask.appendChild(divTask)
-    const taskTitle = document.createElement("h3")
-    taskTitle.textContent = task.title
-    divTask.appendChild(taskTitle)
-    const taskDesc = document.createElement("p")
-    taskDesc.textContent = task.desc
-    divTask.appendChild(taskDesc)
-    const taskDate = document.createElement("h4")
-    taskDate.textContent = task.date
-    divTask.appendChild(taskDate)
+    if (task.priority == filter) {
+      const liTask = document.createElement("li")
+      switch (task.priority) {
+        case 1:
+          liTask.classList.add("green")
+          console.log(task.priority)
+          break
+        case 2:
+          liTask.classList.add("orange")
+          console.log(task.priority)
+          break
+        case 3:
+          liTask.classList.add("red")
+          console.log(task.priority)
+          break
+        default:
+          liTask.classList.add("blue")
+          console.log(task.priority)
+      }
+      ulTask.appendChild(liTask)
+      const taskCheck = document.createElement("input")
+      taskCheck.type = "checkbox"
+      taskCheck.id = task.id
+      taskCheck.name = task.title
+      console.log(task.priority)
+      taskCheck.addEventListener("mouseenter", () => {
+        taskCheck.checked = true
+      })
+      taskCheck.addEventListener("mouseleave", () => {
+        taskCheck.checked = false
+      })
+      taskCheck.addEventListener("click", () => {
+        localStorage.removeItem(task.id)
+        displayTasks(divContent)
+        const buttonAddTask = document.querySelector(".add-task")
+        divContent.removeChild(buttonAddTask)
+        createButtonAddTask(divContent)
+      })
+      liTask.appendChild(taskCheck)
+      const divTask = document.createElement("div")
+      divTask.classList.add("task-details")
+      liTask.appendChild(divTask)
+      const taskTitle = document.createElement("h3")
+      taskTitle.textContent = task.title
+      divTask.appendChild(taskTitle)
+      const taskDesc = document.createElement("p")
+      taskDesc.textContent = task.desc
+      divTask.appendChild(taskDesc)
+      const taskDate = document.createElement("h4")
+      taskDate.textContent = task.date
+      divTask.appendChild(taskDate)
 
-    divContent.appendChild(ulTask)
+      divContent.appendChild(ulTask)
+    }
   })
 }
 

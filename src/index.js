521fc0cd8aa834a685303bea18eb70250998b570
zeratheuz <zeratheuz@gmail.com>
2025-10-04 17:55:47 -0300

    Update JS

--- src/index.js
+++ src/index.js
@@ -6,13 +6,20 @@ import { createButtonAddTask } from './createButtonAddTask.js'
 createDefaultTasks()
 
 const divContent = document.querySelector("#content")
-
 const filters = Array.from(document.querySelectorAll(".filter"))
+
 filters.forEach(filter => {
   filter.addEventListener("click", () => {
+
     displayTasks(divContent, filter.id)
+
+    const ulTask = document.querySelector("#ulTask")
+    ulTask.className = ""
+    ulTask.classList.add(filter.id)
+
     const buttonAddTask = document.querySelector(".add-task")
     divContent.removeChild(buttonAddTask)
+
     createButtonAddTask(divContent)
   })
 })
--- src/removeTask.js
+++ src/removeTask.js
@@ -3,13 +3,14 @@ import { displayTasks } from "./displayTasks"
 
 export function removeTask(divContent, id) {
 
+  const priority = document.querySelector("#ulTask").className
   const tasks = JSON.parse(localStorage.getItem("tasks"))
   const index = tasks.findIndex(task => task.id == id)
   tasks.splice(index, 1)
 
   localStorage.setItem("tasks", JSON.stringify(tasks))
 
-  displayTasks(divContent)
+  displayTasks(divContent, priority)
   const buttonAddTask = document.querySelector(".add-task")
   divContent.removeChild(buttonAddTask)
   createButtonAddTask(divContent)
